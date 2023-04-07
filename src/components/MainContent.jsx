e3f3f85d7e1df53f5944a50ed93df3e14bc9e887
Rachel Opuba <ogbenirach@gmail.com>
2023-04-07 22:43:37 +0100

    Using state to get input value

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/google-logo.png";
 import searchIcon from "../assets/images/search-icon.png";
 import voiceIcon from "../assets/images/google-voice-icon.png";
@@ -7,6 +7,19 @@ import Icon from "./Icon";
 import Button from "./Button";
 
 const MainContent = (props) => {
+  const [keyword, setKeyword] = useState("");
+  const [updated, setUpdated] = useState("");
+  const handleInput = function (e) {
+    let value = e.target.value;
+    console.log(value);
+    setKeyword(value);
+  };
+
+  const handleEnter = function (e) {
+    if (e.key === "Enter") {
+      setUpdated(keyword);
+    }
+  };
   return (
     <>
       <div className="main-content-container">
@@ -16,17 +29,24 @@ const MainContent = (props) => {
             <Icon img={searchIcon} />
           </div>
           <div className="input">
-            <input type="text" />
+            <input
+              type="text"
+              value={keyword}
+              onChange={handleInput}
+              onKeyDown={handleEnter}
+            />
           </div>
           <div className="right-icon">
             <Icon img={voiceIcon} class={"voice-icon"} />
             <Icon img={lens} class={"lens-icon"} />
           </div>
         </div>
+        <p className="message">{updated}</p>
         <div className="main-buttons">
           <Button text={"Google Search"} />
           <Button text={"I'm Feeling Lucky"} />
         </div>
+
         <div className="languages">
           <span className="lang">Google offered in :</span>
           <a href="#">Hausa</a>
